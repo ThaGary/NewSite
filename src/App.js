812dd079cee2526078f9b5d481a4d7e5e4ac70e2
ThaGary <garyanderson31591@gmail.com>
2018-10-23 15:45:16 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SocialMedia from './content.js';
+import './index.css';
 
 class App extends Component {
   render() {
+    consoleText(['Full-Stack Developer','Gamer','Content Moderator','Snowboarder','Cheez-it Enthusiast'], 'text',['black']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 40)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 100)
+}
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <SocialMedia />
+      </>
     );
   }
 }
---
+++ src/content.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './index.css';
+
+const SocialMedia = () => {
+
+    return ( 
+        <>
+            <div className='container'>
+                <div id='links'>
+                    <nav className="nav flex-column">
+                        <a className="nav-link text-dark" href="https://drive.google.com/file/d/1NThAb6GRNtsRYEaQ5OKAJ3FERUM-MaQs/preview" target="_blank"><i className="fas orange fa-file"></i> Resume</a>
+                        <a className="nav-link text-dark" href="https://www.linkedin.com/in/garyandersoniii/" target="_blank"><i className="fab orange fa-linkedin" ></i> Linkedin</a>
+                        <a className="nav-link text-dark" href="https://github.com/thagary" target="_blank"><i className="fab orange fa-github-square" ></i> Github</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/twitter" target="_blank"><i className="fab orange fa-twitter-square" ></i> Twitter</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/discord" target="_blank"><i className="fab orange fa-discord" ></i> Discord</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/steam" target="_blank"><i className="fab orange fa-steam-square" ></i> Steam</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/instagram" target="_blank"><i className="fab orange fa-instagram" ></i> Instagram</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/twitch" target="_blank"><i className="fab orange fa-twitch" ></i> Twitch</a>
+                    </nav>
+                </div>
+                <div className='side'>
+                <div id='project'>
+                        </div>
+                    <div id='info'>
+                        <p>Junior Full-Stack Developer in Denver actively searching for a job. I currently am attending Galvanize where they are helping me master my skills in JavaScript, React, Node.js and many more. I am a fast learning, team player with attention to detail. I have a tons of experience in live stream broadcasting moderation for online gaming and esports.</p>
+
+                        <p>I actively served in the United States Navy for 4 years as the calibration shop manager for Guam. I spoke at Twitchcon 2017 on "Channel moderation and chat moderation tools" with the lead moderators for ESL, Dreamhack, and a few Twitch Admins. This year I will be attending the Twitchcon Hackathon.</p>
+
+                        <p>When I'm not coding I am watching streams, playing games, hiking, camping, snowboarding, or traveling to the nearest convention.</p>
+                    </div>
+                    <div id='project'>
+                        </div>
+                </div>
+            </div>   
+        </>
+    )
+}
+
+export default SocialMedia
