f90c186b6774aebae92a83fd364a5dc5c8e66008
ThaGary <garyanderson31591@gmail.com>
2018-10-24 15:21:13 -0600

    build site

--- src/content.js
+++ src/content.js
@@ -5,17 +5,17 @@ const SocialMedia = () => {
 
     return ( 
         <>
-            <div className='container'>
+            <div className='container title_message'>
                 <div id='links'>
                     <nav className="nav flex-column">
-                        <a className="nav-link text-dark" href="https://drive.google.com/file/d/1NThAb6GRNtsRYEaQ5OKAJ3FERUM-MaQs/preview" target="_blank"><i className="fas orange fa-file"></i> Resume</a>
-                        <a className="nav-link text-dark" href="https://www.linkedin.com/in/garyandersoniii/" target="_blank"><i className="fab orange fa-linkedin" ></i> Linkedin</a>
-                        <a className="nav-link text-dark" href="https://github.com/thagary" target="_blank"><i className="fab orange fa-github-square" ></i> Github</a>
-                        <a className="nav-link text-dark" href="https://thagary.com/twitter" target="_blank"><i className="fab orange fa-twitter-square" ></i> Twitter</a>
-                        <a className="nav-link text-dark" href="https://thagary.com/discord" target="_blank"><i className="fab orange fa-discord" ></i> Discord</a>
-                        <a className="nav-link text-dark" href="https://thagary.com/steam" target="_blank"><i className="fab orange fa-steam-square" ></i> Steam</a>
-                        <a className="nav-link text-dark" href="https://thagary.com/instagram" target="_blank"><i className="fab orange fa-instagram" ></i> Instagram</a>
-                        <a className="nav-link text-dark" href="https://thagary.com/twitch" target="_blank"><i className="fab orange fa-twitch" ></i> Twitch</a>
+                        <a className="nav-link text-dark" href="https://drive.google.com/file/d/1NThAb6GRNtsRYEaQ5OKAJ3FERUM-MaQs/preview" target="_blank"><i className="fas fa-2x orange fa-file"></i> Resume</a>
+                        <a className="nav-link text-dark" href="https://www.linkedin.com/in/garyandersoniii/" target="_blank"><i className="fab fa-2x orange fa-linkedin" ></i> Linkedin</a>
+                        <a className="nav-link text-dark" href="https://github.com/thagary" target="_blank"><i className="fab fa-2x orange fa-github-square" ></i> Github</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/twitter" target="_blank"><i className="fab fa-2x orange fa-twitter-square" ></i> Twitter</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/discord" target="_blank"><i className="fab fa-2x orange fa-discord" ></i> Discord</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/steam" target="_blank"><i className="fab fa-2x orange fa-steam-square" ></i> Steam</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/instagram" target="_blank"><i className="fab fa-2x orange fa-instagram" ></i> Instagram</a>
+                        <a className="nav-link text-dark" href="https://thagary.com/twitch" target="_blank"><i className="fab fa-2x orange fa-twitch" ></i> Twitch</a>
                     </nav>
                 </div>
                 <div className='side'>
