67baadd6e350e0c8b26496d30cc2809c33dce90e
ThaGary <garyanderson31591@gmail.com>
2019-04-12 14:52:04 -0600

    personal site redesign, created new Nav

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,8 @@
-import React from 'react';
-import SocialMedia from './SocialMedia';
+import React from 'react'
 
 const Content = (props) => {
     return ( 
         <div className="container">
-            <SocialMedia handleClick={props.handleClick} />
-            <div>
-                <h1 className="orange">Gary Anderson</h1>
-                <div className="spacer"></div> 
-          <img src="https://i.imgur.com/d72ET64.jpg" width="200em" align="right" alt="me" />
-                <div className="aboutme">
-                    <p>Junior Full-Stack Developer in Denver actively searching for a job. I currently am attending Galvanize where they are helping me master my skills in JavaScript, React, Node.js and many more. I am a fast learning, team player with attention to detail. I have a tons of experience in live stream broadcasting moderation for online gaming and esports.I actively served in the United States Navy for 4 years as the calibration shop manager for Guam. I spoke at Twitchcon 2017 on "Channel moderation and chat moderation tools" with the lead moderators for ESL, Dreamhack, and a few Twitch Admins. This year I will be attending the Twitchcon Hackathon.When I'm not coding I am watching streams, playing games, hiking, camping, snowboarding, or traveling to the nearest convention.</p>
-                </div>
-            </div>
         </div>
     )
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css';
+
+const Nav = (props) => {
+    return ( 
+        <div className="nav">
+            <div className="nav__links">
+                <a href="google.com">Home </a>
+                <a href="google.com">Journey </a>
+                <a href="google.com">Contact </a>
+            </div>
+            <div className="nav__line"></div>
+        </div>
+    )
+}
+
+export default Nav
