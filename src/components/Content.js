8afdddefc69cb067ccfedfd1c6e579915fa0f0c2
ThaGary <garyanderson31591@gmail.com>
2019-04-12 16:02:52 -0600

    added picture with animation and layout of landing page

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Content.css';
 
 const Content = (props) => {
     return ( 
-        <div className="container">
+        <div className="content">
+            <div className="landing">
+                <div className="left">
+                    <img className="picture" src="https://i.imgur.com/d72ET64.jpg" alt=""/>
+                </div>
+                <div className="right">
+                    <div className="name">Gary Anderson</div>
+                    <div className="title">
+                    <p>Web Developer</p>
+                    <p>Content Moderator</p>
+                    <p>Risk Analyst</p></div>
+                </div>
+                
+            </div>
         </div>
     )
 }
